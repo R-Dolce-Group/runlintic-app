5e44abec289327ac352fe22a6d30132860a04287
Marti D <marti@rdolcegroup.com>
2025-09-01 14:54:30 -0500

    feat: add initial runlintic-app.js script with file header documentation

---
+++ bin/runlintic-app.js
@@ -0,0 +1,10 @@
+/** filepath/filename.js
+ *
+ * Function ---------------
+ * What does this file do?
+ * Why is it required?
+ * ------------------------
+ *
+ * Author| workflow
+ * mrsdo@29signals.com | https://github.com/mrsdo
+ */
